8bd5182b0e7b30d65a9a6467f43e9fa9f76f5a4d
Jacquais <24923777+Jacquais@users.noreply.github.com>
2017-09-02 17:58:21 -0400

    Added more

--- PwnMe.py
+++ PwnMe.py
@@ -2,13 +2,15 @@ import argparse
 import io
 import itertools
 import os
+from pprint import pprint
+
 os.system('cls')
 parser = argparse.ArgumentParser(description='A script to perform bruteforce dictionary attacks on brainwallets.')
 parser.add_argument('-p', action='store', dest='password',
                     help='Password that the script is guessing', required=True)
 parser.add_argument('-i', action='store', nargs='+', dest='info',
-                    help='Relevant information that someone might have about you. ex: birthday, dogs name, childhood friend, your name.\n'
-                         'These should be separated by commas. ex: September 24 Spot Johnny Appleseed Jon Doe')
+                    help='Relevant information that someone might have about you. ex: birthday dogs name childhood friend your name.\n'
+                         'These should be separated by Spaces. ex: September 24 Spot Johnny Appleseed Jon Doe')
 args = parser.parse_args()
 
 print("This Script will attempt to guess the password you supply.\nThis can be used to evaluate how easily your password can be 'pwnend'.")
@@ -33,15 +35,17 @@ for raw_word in dict_file:
         passFound = True
         print("\n\n\nFOUND! Is your password {}?".format(dictionary_word))
         break
-if not passFound and args.info != None:
+if not passFound and args.info is not None:
     print("Password not found using word list, trying info user provided.")
-    userInfoList = list(itertools.product(args.info, repeat=len(args.info)))
-    print(len(userInfoList))
+    userInfoList = []
+    for i in range(len(args.info)):
+        InfoList = list(itertools.product(args.info, repeat=i))
+        userInfoList.append(InfoList)
+    userInfoList = list(itertools.chain.from_iterable(userInfoList))
     for x in range(len(userInfoList)):
-        print(''.join(userInfoList[x]))
         if ''.join(userInfoList[x]) == args.password:
             print("\n\n\nFOUND! Is your password {}?".format(''.join(userInfoList[x])))
             break
-elif not passFound:
+elif not passFound and args.info is None:
     print("Password not found.\nTry adding relevent info that an attacker may know using the -i argument")
 dict_file.close()
